2ec677c7f213d0c60dc125ad3add7e21987619c4
Mancorl <133156710+Mancorl@users.noreply.github.com>
2023-05-29 23:09:07 +0200

    Add files via upload
    Filene til fourriertransformator og feilet forsøk på neural net. for å kjøre programmet bruk Fourriertransformator.exe det følger med en lydfil for testingsom /lyd/test.wav (alle testfiler må være .wav filer i mono ikke stereo)

---
+++ Phys117oppgave/Phys117oppgave/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Public Property Label1 As Object
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FileNum As Integer = FreeFile()
+        Dim TempS As String = ""
+        Dim TempL As String
+        FileOpen(FileNum, "error.txt", OpenMode.Input)
+        Do Until EOF(FileNum)
+            TempL = LineInput(FileNum)
+            TempS += TempL + vbCrLf
+        Loop
+        FileClose(FileNum)
+        Label1.Text = TempS
+    End Sub
+End Class
